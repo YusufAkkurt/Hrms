f5ff11198e1a942a14363a40093d0674e2d32057
Yusuf Akkurt <6276592-yusuf-akkurt@users.noreply.gitlab.com>
2021-05-10 01:39:51 +0300

    JobPosition Entity'si oluşuturldu. Lombok ile getter ve setter'lar eklendi. Lombok ile parametreli ve parametresiz constructor'lar eklendi.

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/JobPosition.java
@@ -0,0 +1,29 @@
+package kodlamaio.hrms.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "job_positions")
+public class JobPosition {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "active")
+    private boolean active;
+}
