82c97a83d6723bc4c805b76ad74d89fab6abcfea
Yusuf Akkurt <6276592-yusuf-akkurt@users.noreply.gitlab.com>
2021-05-23 20:51:40 +0300

    User service oluşturuldu

---
+++ src/main/java/kodlamaio/hrms/business/abstracts/UserService.java
@@ -0,0 +1,16 @@
+package kodlamaio.hrms.business.abstracts;
+
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.abstracts.User;
+
+import java.util.List;
+
+public interface UserService {
+    DataResult<List<User>> getAll();
+    DataResult<User> getByEmail(String email);
+
+    Result add(User user);
+    Result update(User user);
+    Result delete(User user);
+}
