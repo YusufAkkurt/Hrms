446e17599442e5bb0466ad19a2e9cc62d1b0431f
Yusuf Akkurt <6276592-yusuf-akkurt@users.noreply.gitlab.com>
2021-05-28 17:56:57 +0300

    JobAdDao parametre güncellendi

--- src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobAdDao.java
+++ src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobAdDao.java
@@ -4,10 +4,11 @@ import kodlamaio.hrms.entities.concretes.JobAd;
 import kodlamaio.hrms.entities.dtos.JobAdDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface JobAdDao extends JpaRepository<JobAd, Integer> {
     @Query("select new kodlamaio.hrms.entities.dtos.JobAdDto(JobAd.id, jobAd.employer.companyName, jobAd.job.name, jobAd.openPositionCount, jobAd.createdAt, jobAd.applicationDeadline) from JobAd jobAd where jobAd.active = :active")
-    List<JobAdDto> dtoGetJobAdsByActiveIs();
+    List<JobAdDto> dtoGetJobAdsByActiveIs(@Param("active") boolean active);
 }
