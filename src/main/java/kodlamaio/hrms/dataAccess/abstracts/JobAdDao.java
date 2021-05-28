a8197c6251c530186810c6f5746f9a102af4cb8e
Yusuf Akkurt <6276592-yusuf-akkurt@users.noreply.gitlab.com>
2021-05-28 17:46:59 +0300

    JobAdDao isimlendirme güncellendi

--- src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobAdDao.java
+++ src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobAdDao.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface JobAdDao extends JpaRepository<JobAd, Integer> {
-    @Query("select new kodlamaio.hrms.entities.dtos.JobAdDto(JobAd.id, jobAd.employer.companyName, jobAd.job.name, jobAd.openPositionCount, jobAd.createdAt, jobAd.applicationDeadline) from JobAd jobAd where jobAd.active = true")
-    List<JobAdDto> retrieveJobAdsAsActiveDto();
+    @Query("select new kodlamaio.hrms.entities.dtos.JobAdDto(JobAd.id, jobAd.employer.companyName, jobAd.job.name, jobAd.openPositionCount, jobAd.createdAt, jobAd.applicationDeadline) from JobAd jobAd where jobAd.active = :active")
+    List<JobAdDto> dtoGetJobAdsByActiveIs();
 }
