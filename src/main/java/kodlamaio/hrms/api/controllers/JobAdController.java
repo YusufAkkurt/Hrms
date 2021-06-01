8aa3b86a4f9eaf6f05fd0d0d2f65e5562646a6e9
Yusuf Akkurt <6276592-yusuf-akkurt@users.noreply.gitlab.com>
2021-06-01 18:33:44 +0300

    Bug fixed

--- src/main/java/kodlamaio/hrms/api/controllers/JobAdController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/JobAdController.java
@@ -1,6 +1,6 @@
 package kodlamaio.hrms.api.controllers;
 
-import kodlamaio.hrms.business.concretes.JobAdManager;
+import kodlamaio.hrms.business.abstracts.JobAdService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.JobAd;
@@ -13,35 +13,35 @@ import java.util.List;
 @RestController
 @RequestMapping("api/jobAds")
 public class JobAdController {
-    private final JobAdManager jobAdManager;
+    private final JobAdService jobAdService;
 
     @Autowired
-    public JobAdController(JobAdManager jobAdManager) {
-        this.jobAdManager = jobAdManager;
+    public JobAdController(JobAdService jobAdService) {
+        this.jobAdService = jobAdService;
     }
 
     @GetMapping("getAll")
     private DataResult<List<JobAdDto>> dtoGetJobAdsByActiveIs(@RequestParam boolean status) {
-        return this.jobAdManager.dtoGetJobAdsByActiveIs(status);
+        return this.jobAdService.dtoGetJobAdsByActiveIs(status);
     }
 
     @GetMapping("getByEmployerId")
     private DataResult<List<JobAdDto>> dtoGetJobAdsByActiveAndEmployerId(@RequestParam int employerId) {
-        return this.jobAdManager.dtoGetJobAdsByActiveAndEmployerId(employerId);
+        return this.jobAdService.dtoGetJobAdsByActiveAndEmployerId(employerId);
     }
 
     @GetMapping("getAllReverseSortedByCreatedAt")
     private DataResult<List<JobAdDto>> dtoGetJobAdsByActiveReverseSort() {
-        return this.jobAdManager.dtoGetJobAdsByActiveReverseSort();
+        return this.jobAdService.dtoGetJobAdsByActiveReverseSort();
     }
 
     @PostMapping
     private Result add(@RequestBody JobAd jobAd) {
-        return this.jobAdManager.add(jobAd);
+        return this.jobAdService.add(jobAd);
     }
 
     @PostMapping("doPassive")
     private Result add(@RequestBody int jobAdId) {
-        return this.jobAdManager.doPassive(jobAdId);
+        return this.jobAdService.doPassive(jobAdId);
     }
 }
